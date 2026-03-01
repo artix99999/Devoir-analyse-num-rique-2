a7609771b434d64e76f005b7a86bd93f5b00fedc
Jeanne Ducré <jeduc55@ulaval.ca>
2026-03-01 15:38:31 -0500

    ajout des question l et m

--- equipe40.py
+++ equipe40.py
@@ -70,3 +70,35 @@ print_table(its_g1, "Tableau (g1) : g1(Q)=ln(-Q/2+5)")
 # Tableau g2
 its_g2 = pointfixe(g2, Q0, tolr, nmax)
 print_table(its_g2, "Tableau (g2) : g2(Q)=10-2e^Q (Q0 à Q5)", max_n=5)
+
+# Question l)
+def g3(Q):
+    return Q - (np.exp(Q) + Q/2 - 5) / (np.exp(Q) + 1/2)
+
+#Tableau g3
+its_g3 =pointfixe(g3, Q0, tolr, nmax)
+print_table(its_g3, "Tableau (g3) : g3(Q)=Q + (e^Q + Q/2 - 5)/(e^Q + 1/2)", max_n=10)
+
+#Question m)
+def g1_stef(Q):
+    gQ = g1(Q)
+    ggQ = g1(gQ)
+    return Q - (gQ - Q)**2 / (ggQ - 2*gQ + Q)
+
+def g2_stef(Q):
+    gQ = g2(Q)
+    ggQ = g2(gQ)
+    return Q - (gQ - Q)**2 / (ggQ - 2*gQ + Q)
+
+def gN_stef(Q):
+    gQ = g3(Q)
+    ggQ = g3(gQ)
+    return Q - (gQ - Q)**2 / (ggQ - 2*gQ + Q)
+
+its_g1Steff = pointfixe(g1_stef, Q0, tolr, nmax)
+print_table(its_g1Steff, "Tableau (g1_stef) : g1_stef(Q)=Q - (g1(Q) - Q)^2 / (g1(g1(Q)) - 2*g1(Q) + Q)", max_n=10)
+its_g2Steff = pointfixe(g2_stef, Q0, tolr, nmax)
+print_table(its_g2Steff, "Tableau (g2_stef) : g2_stef(Q)=Q - (g2(Q) - Q)^2 / (g2(g2(Q)) - 2*g2(Q) + Q)", max_n=10)
+its_g3Steff = pointfixe(gN_stef, Q0, tolr, nmax)
+print_table(its_g3Steff, "Tableau (g3_stef) : g3_stef(Q)=Q - (g3(Q) - Q)^2 / (g3(g3(Q)) - 2*g3(Q) + Q)", max_n=10)
+
